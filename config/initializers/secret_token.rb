f55e971dfff7c662e5be7d7575ab12c15c77d7a0
Eric Perkins <e.perkins@n2neuron.com>
2013-09-07 10:36:20 -0500

    Empty rails 4 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Deepvision::Application.config.secret_key_base = '106dcec8cc31cb154168b22150f4b17e5cc4f01784e4e75f0538addb8c5bc215431eb17cddb90b2a453493303385825b71524ae85edb48171101f0470a2d6a2a'
