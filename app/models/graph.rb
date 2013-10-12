e39401838932b59001781db6b73717bbebd1b96e
Eric Perkins <e.perkins@n2neuron.com>
2013-10-12 15:09:49 -0500

    DRY-ing up some code.

--- app/models/graph.rb
+++ app/models/graph.rb
@@ -1,17 +1,15 @@
 class Graph
 
   def self.get_cpu_graph_series(longterm_stats)
-    series = Array.new
-
-    longterm_stats.each do |longterm|
-      series << [ longterm["timestamp"].to_i * 1000, longterm["CPU.total.usage"] ]
-    end
-
-    series
+    Graph.get_graph_series(longterm_stats, "CPU.total.usage")
   end
 
   def self.get_load_graph_series(longterm_stats)
-    longterm_stats.map {|longterm| [longterm["timestamp"].to_i * 1000, longterm["Load"]]}
+    Graph.get_graph_series(longterm_stats, "Load")
+  end
+
+  def self.get_graph_series(longterm_stats, attribute)
+    longterm_stats.map {|longterm| [longterm["timestamp"].to_i * 1000, longterm[attribute]]}
   end
 
 end
