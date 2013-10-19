a98107b26174a6f1bcd089702f584b9f2a3e63a1
Eric Perkins <e.perkins@n2neuron.com>
2013-10-19 11:15:38 -0500

    Requiring ActiveSupport

--- config/application.rb
+++ config/application.rb
@@ -3,6 +3,7 @@ require File.expand_path('../boot', __FILE__)
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "active_model/railtie"
+require "active_support/railtie"
 require "rails/test_unit/railtie"
 require "sprockets/railtie"
 
