bcfa491d365421b83801416ae0dfdcbbfd9a123b
Eric Perkins <e.perkins@n2neuron.com>
2013-11-05 23:33:08 -0600

    Adding debugger logs to rethinkdb lovelyrethink

--- lib/lovely_rethink/connection.rb
+++ lib/lovely_rethink/connection.rb
@@ -7,11 +7,13 @@ module LovelyRethink
     attr_accessor :uri, :host, :port, :database_name
 
     def initialize(uri)
+      Rails.logger.debug 'initializing rethinkdb'
       self.uri = uri
       parse_uri
     end
 
     def raw
+      Rails.logger.debug 'calling rethinkdb raw'
       @raw ||= RethinkDB::Connection.new(:host => host, :port => port, :db => database_name).repl
     end
 
@@ -21,6 +23,7 @@ module LovelyRethink
     private
 
     def parse_uri
+      Rails.logger.debug 'parsing uri'
       require 'uri'
       parsed_uri = URI.parse(uri)
 
