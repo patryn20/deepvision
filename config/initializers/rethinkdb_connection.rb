f9ac38add791111621614117e4479d8a14ec1949
Eric Perkins <e.perkins@n2neuron.com>
2013-11-09 18:02:02 -0600

    Removing old todos

--- config/initializers/rethinkdb_connection.rb
+++ config/initializers/rethinkdb_connection.rb
@@ -1,7 +1,3 @@
 require 'lovelyrethink'
 
-#this is not threadsafe and the connection can go away before the next request is processed
-#@TODO need to extend lovelyrethink to detect connection failures and reconnect
-#@TODO also need to extend the object to run the queries with the connection included automatically
-#@TODO maybe something like LovelyRethink.run(@r.table().order.blah)
 LovelyRethink.configure 'rethinkdb://localhost:28015/deepvision'
