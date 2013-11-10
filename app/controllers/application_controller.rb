a666770f7db3f69841cf860b8384a3ed25d51b09
Eric Perkins <e.perkins@n2neuron.com>
2013-11-09 18:11:01 -0600

    Removing commented out code.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  #before_filter :connect_to_rethinkdb
-
-  #protected
-  #def connect_to_rethinkdb
-  #  #LovelyRethink.connect 'rethinkdb://127.0.0.1:28015/deepvision'
-  #end
 end
