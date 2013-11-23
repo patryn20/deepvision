b50f9b540c09d40ac9da1d88d1ecba1291ed4b14
Eric Perkins <e.perkins@n2neuron.com>
2013-11-23 17:17:47 -0600

    Removing unused count of all longterm objects.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,9 +3,7 @@ class DashboardController < ApplicationController
   def index
     @dashboard = true
 
-    @r = LovelyRethink.db
-
-    @longterms = @r.table('longterm').count.run(LovelyRethink.connection.raw)
+    @r = LovelyRethink.db    
 
     @last_longterms = Longterm.get_all_most_recent
 
